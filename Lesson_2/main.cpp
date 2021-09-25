3833a088c452cebde5550d0587ef1a2f03c716f8
Kirill Cheburkov <junskyx@gmail.com>
2021-09-25 23:27:51 +0300

    Домашнее задание Урок 2

---
+++ Lesson_2/main.cpp
@@ -0,0 +1,26 @@
+// написать программу решающую математическую функцию
+// условие в телеграмме
+
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+   double x;
+   printf ( "Plese enter x: ");
+   scanf ( "%1x", &x );
+
+
+
+   const double hisl = (2 * cos( x - M_PI / 6.)) + sqrt( 2 );
+   const double znam = (1 /( 2 * log (x))) + pow(sin (x*x),2 );
+   const double result = exp(3*x)*(hisl/znam);
+
+   printf("Result Y= %f\n", result);
+
+
+
+
+
+    return 0;
+}
