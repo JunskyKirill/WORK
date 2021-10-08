823b5ba68874d1f73fb90a816a908ac93591d2fb
Kirill Cheburkov <junskyx@gmail.com>
2021-10-09 00:29:49 +0300

    Домашнее Задание 2

--- Lesson_3_3/main.cpp
+++ Lesson_3_3/main.cpp
@@ -1,21 +1,60 @@
+// Найти наименьший общий делитель трех натуральных чисел
+
 #include <stdio.h>
 #include <math.h>
 
 
 int main()
 {
-    double n;
-    printf("Please enter n : ");
-    scanf ("%lf", &n);
+    int n1,n2,n3;
 
-    int x = 1;
+    printf("Please enter n1 : \n");
+    scanf ("%d", &n1);
+    printf("Please enter n2 : \n");
+    scanf ("%d", &n2);
+    printf("Please enter n3 : \n");
+    scanf ("%d", &n3);
 
-    while ((n/=10) > 1)
+    int inputedMin = n1 > n2 ? (n2 > n3 ? n3 : n2) : (n1 > n3 ? n3 : n1); // Минимальное введенное число
+    bool hasCommonDiv; // Является ли минимальное число общим делителем
+    int div = 2; // минимальный делитель
+
+    //hasCommonDiv = n1 % div == 0 && n2 % div == 0 && n3 % div == 0;
+    while (div <= inputedMin && !hasCommonDiv)
     {
-        x++;
+        hasCommonDiv = n1 % div == 0 && n2 % div == 0 && n3 % div == 0;
+        if (hasCommonDiv)
+        {
+            break;
+        }
+
+        ++div;
     }
 
-    printf("N = %d", x);
+    if (!hasCommonDiv)
+    {
+        div = 1;
+    }
+
+    /*if (hasCommonDiv)
+    {
+        div = inputedMin;
+    }
+    else
+    {
+        for (div = 2; div < inputedMin && !hasCommonDiv; ++div)
+        {
+            hasCommonDiv = n1 % div == 0 && n2 % div == 0 && n3 % div == 0;
+            if (hasCommonDiv)
+            {
+                break;
+            }
+        }
+
+
+    }*/
+
+    printf("Min divider = %d", div);
 
 
 
